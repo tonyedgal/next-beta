d9ac0cb8da989cf1aad10707dbf596219091d768
tonyedgal <tonyedgal@gmail.com>
2023-05-03 23:47:04 +0100

    feat: implement suspense in data fetching

--- app/(users)/todos/TodosList.tsx
+++ app/(users)/todos/TodosList.tsx
@@ -3,6 +3,10 @@ import { Todo } from "@/typings";
 import Link from "next/link";
 
 const fetchTodos = async () => {
+  // timeout for random number of seconds btw 1 and 5
+  const timeout = Math.floor(Math.random() * 5 + 1) * 1000;
+  await new Promise((resolve) => setTimeout(resolve, timeout));
+
   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
   const todos: Todo[] = await res.json();
   return todos;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
+import { Suspense } from "react";
+import TodosList from "./(users)/todos/TodosList";
+
 export default function Home() {
-  return <h1>Hi Next JS</h1>;
+  return (
+    <div>
+      <Suspense fallback={<p className="text-red-500">Loading Todos...</p>}>
+        <h1>Loading Todos</h1>
+        <div className="flex space-x-2">
+          {/* @ts-ignore */}
+          <TodosList />
+        </div>
+      </Suspense>
+
+      <Suspense fallback={<p className="text-blue-500">Loading Trolleys...</p>}>
+        <h1>Loading Shopping Trolley</h1>
+        <div className="flex space-x-2">
+          {/* @ts-ignore */}
+          <TodosList />
+        </div>
+      </Suspense>
+    </div>
+  );
 }
