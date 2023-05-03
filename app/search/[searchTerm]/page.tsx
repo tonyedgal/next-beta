cec17e8620789a00df1ff7471d83b720814d11f9
tonyedgal <tonyedgal@gmail.com>
2023-05-03 23:17:27 +0100

    feat: error handling

--- app/search/[searchTerm]/page.tsx
+++ app/search/[searchTerm]/page.tsx
@@ -22,6 +22,8 @@ const search = async (searchTerm: string) => {
   const res = await fetch(
     `https://serpapi.com/search.json?q=${searchTerm}&api_key=${process.env.API_KEY}`
   );
+
+  throw new Error("Something went wrong");
   const data: SearchResult = await res.json();
   return data;
 };
