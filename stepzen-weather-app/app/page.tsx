ecfd11b8f1e390914c501432ff653d897c8e3318
tonyedgal <tonyedgal@gmail.com>
2023-05-18 21:45:11 +0100

    feat: designed home page layout

--- stepzen-weather-app/app/page.tsx
+++ stepzen-weather-app/app/page.tsx
@@ -1,9 +1,19 @@
 "use client";
 
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
+
 export default function Home() {
   return (
-    <main className="text-6xl">
-      <h1>Lets build a GPT-4 weather app</h1>
+    <main className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
+      <Card className="max-w-4xl mx-auto">
+        <Text className="text-6xl font-bold text-center mb-5">Weather AI</Text>
+        <Subtitle className="text-xl text-center">Powered by OpenAI</Subtitle>
+        <Divider className="my-5" />
+
+        <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
+          {/** City Picker */}
+        </Card>
+      </Card>
     </main>
   );
 }
