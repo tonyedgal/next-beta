be1dcc98cee269f81e7aff1864afb2364e13f292
tonyedgal <tonyedgal@gmail.com>
2023-05-21 16:10:50 +0100

    feat: add two required dependencies
    - add `Country-State-City` and `React-Select` dependencies

---
+++ stepzen-weather-app/components/CityPicker.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { Country, City } from "country-state-city";
+import Select from "react-select";
+
+function CityPicker() {
+  return <div>CityPicker</div>;
+}
+
+export default CityPicker;
