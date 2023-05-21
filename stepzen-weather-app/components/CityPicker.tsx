e51dfe8801a25a7250dcf4c628eb29489b26fd09
tonyedgal <tonyedgal@gmail.com>
2023-05-21 16:15:00 +0100

    feat: add react select drop down for all countries

--- stepzen-weather-app/components/CityPicker.tsx
+++ stepzen-weather-app/components/CityPicker.tsx
@@ -3,8 +3,21 @@
 import { Country, City } from "country-state-city";
 import Select from "react-select";
 
+const options = Country.getAllCountries().map((country) => ({
+  value: {
+    latitude: country.latitude,
+    longitude: country.longitude,
+    isoCode: country.isoCode,
+  },
+  label: country.name,
+}));
+
 function CityPicker() {
-  return <div>CityPicker</div>;
+  return (
+    <div>
+      <Select options={options} />
+    </div>
+  );
 }
 
 export default CityPicker;
